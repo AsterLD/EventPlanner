cd3b6dfe521f9734e5dc589b7103cace3cc91e32
Lazarev.D <Lazarev.d.g@yandex.ru>
2023-07-23 00:07:03 +0300

    C6 - Fixed KafkaConsumer.

--- src/main/java/com/ld/notificator/kafka/KafkaConsumer.java
+++ src/main/java/com/ld/notificator/kafka/KafkaConsumer.java
@@ -1,11 +1,14 @@
 package com.ld.notificator.kafka;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class KafkaConsumer {
+    @KafkaListener(topics = "notificator",
+            groupId = "notificator-listener")
     public void consume(String message){
         log.info("Message received: " + message);
     }

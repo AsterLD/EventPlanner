c36c7033ff8692cabc8523de0a165d178ccce541
Lazarev.D <Lazarev.d.g@yandex.ru>
2023-07-24 23:55:14 +0300

    C7 - Added Event entity, repository and migration.

---
+++ src/main/java/com/ld/notificator/entity/Event.java
@@ -0,0 +1,27 @@
+package com.ld.notificator.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Table(name = "events")
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "event_id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "event_date")
+    private LocalDate eventDate;
+
+}

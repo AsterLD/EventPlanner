cb97fe6092e1aea1ce2e5bbcca4ab8c372f7db41
Lazarev.D <Lazarev.d.g@yandex.ru>
2023-08-05 00:35:29 +0300

    C13 - Fixed kafka properties in application.yml, Added new beans in KafkaConfig.

--- src/main/java/com/ld/notificator/config/KafkaConfig.java
+++ src/main/java/com/ld/notificator/config/KafkaConfig.java
@@ -1,14 +1,29 @@
 package com.ld.notificator.config;
 
+import com.ld.notificator.dto.EventToApproveDTO;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 
 @Configuration
 public class KafkaConfig {
     @Bean
     public NewTopic topic() {
         return TopicBuilder.name("notificator").build();
+    }
+
+    @Bean
+    public KafkaTemplate<String, EventToApproveDTO> kafkaTemplate(ProducerFactory<String, EventToApproveDTO> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
+
+    @Bean
+    public ProducerFactory<String, EventToApproveDTO> producerFactory(KafkaProperties kafkaProperties) {
+        return new DefaultKafkaProducerFactory<>(kafkaProperties.buildProducerProperties());
     }
 }
